af3cb9c1e7b1cb7d780bfadd80e23a245ab64fba
3a1b2c3@protonmail.com <kabischmid@gmail.com>
2026-08-29 11:13:36 +1000

    merge and fix

--- integrations/lingbot/run.sh
+++ integrations/lingbot/run.sh
@@ -7,9 +7,9 @@ cd "$HERE"
 source .venv/bin/activate
 
 flashdreams-run lingbot-world-fast webrtc \
-  --host 0.0.0.0 \
-  --port 8089 \
-  --device cuda:0 \
-  --example-idx 0 \
-  --warmup-chunks 0
+  --host=0.0.0.0 \
+  --port=8089 \
+  --device=cuda:0 \
+  --example-idx=0 \
+  --output.warmup-chunks=0
 
