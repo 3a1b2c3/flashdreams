aaca3cc4291aa45f17f42ba83bd61a98c94e15bc
3a1b2c3@protonmail.com <kabischmid@gmail.com>
2026-08-29 10:52:25 +1000

    merge and fix

--- integrations/lingbot/run.sh
+++ integrations/lingbot/run.sh
@@ -9,8 +9,8 @@ source .venv/bin/activate
 
 export PYTHONPATH="$FLASHDREAMS_ROOT:$HERE:$FLASHDREAMS_ROOT/apps:${PYTHONPATH:-}"
 
-python -c "
+python << 'PYEOF'
 from lingbot.demo.app import main
 main(['webrtc', '--host', '0.0.0.0', '--port', '8089', '--device', 'cuda:0', '--example-data', 'True'])
-"
+PYEOF
 
