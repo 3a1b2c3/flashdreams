23b44157f70591c4d0e7af452b8d608888ba628d
3a1b2c3@protonmail.com <kabischmid@gmail.com>
2026-08-29 11:38:22 +1000

    merge and fix

--- integrations/lingbot/lingbot/runner.py
+++ integrations/lingbot/lingbot/runner.py
@@ -24,10 +24,7 @@ from typing import Annotated
 import tyro
 from loguru import logger
 
-try:
-    from flashdreams.runtime_v2.video_tensor import VideoTensorLayout
-except ImportError:
-    from flashdreams.infra.postprocess import VideoTensorLayout
+from flashdreams.runtime_v2.video_tensor import VideoTensorLayout
 from flashdreams.infra.runner import Runner, RunnerConfig
 from flashdreams.infra.runner_io import runner_artifact_path
 from flashdreams.runtime.demo import DemoSpec, Mp4OutputSpec, OutputSpec
